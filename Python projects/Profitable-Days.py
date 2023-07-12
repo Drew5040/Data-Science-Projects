26971e1fef18df4573ff7de5d324f747efc9ea3d
Andrew O'Drain <86028354+Drew5040@users.noreply.github.com>
2023-07-12 07:46:31 -0400

    Update Profitable-Days.py

--- Python projects/Profitable-Days.py
+++ Python projects/Profitable-Days.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-# Read in data from CSv
+# Read in data from CSV
 df = pd.read_csv("../Log.Rainbow.Regression/LINK-USD.csv")
 
 # Convert Date-time to pandas readable format
@@ -12,21 +12,18 @@ df["Date"] = pd.to_datetime(df["Date"])
 # Grab latest price
 latest_price = df["Value"].iloc[1957]
 
-# Creates a profit column in our dataframe
+# Create profit column in our dataframe
 df["Profit"] = df["Value"]
 
-# Find where BTC was not profitable and remove the value from profit column "inplace"ensures changes to this dataframe
+# Find where BTC was not profitable and remove the values from profit column
 df["Profit"].where(df["Value"] < latest_price, None, inplace=True)
 
-# PLOT
 
 # Create Chart
 fig = go.Figure()
 
-# Trace is any sort of line or scatter graph (Line graphs are a subset of scatter graphs in plotly
-# Create X any Y axis
-# First Trace draws a red line graph on which we then will draw a green graph over top of red graph
 
+# Add first trace
 fig.add_trace(go.Scatter(
     x=df["Date"],
     y=df["Value"],
@@ -34,7 +31,7 @@ fig.add_trace(go.Scatter(
     line=dict(color="red")
 ))
 
-# Since this Trace is lower down in the program it will be drawn on top of the first trace
+# Add top trace
 fig.add_trace(go.Scatter(
     x=df["Date"],
     y=df["Profit"],
@@ -42,38 +39,43 @@ fig.add_trace(go.Scatter(
     line=dict(color="green")
 ))
 
-# Add dynamic annotation that changes when data gets updated
-# Show arrow can be used to point to a specific location on graph
-# xref and yref allows us to locate the annotation as a percentage of the graph
-
+# Create variables for the dynamic annotation
 total_days = len(df)
 total_profitable_days = len(df[df["Profit"] < latest_price])
 percent_profitable = round(total_profitable_days / total_days * 100, 2)
 
+# Add total days annotation
 fig.add_annotation(text=f"Total Days:  {total_days}",
                    xref='paper', yref="paper",
                    x=0.05, y=0.9, showarrow=False,
                    font=dict(size=20))
 
+# Add total profitable days annotation
 fig.add_annotation(text=f"Total Profitable Days:  {total_profitable_days}",
                    xref='paper', yref="paper",
                    x=0.05, y=0.80, showarrow=False,
                    font=dict(size=20))
 
+# Add percentage of days that were profitable annotation
 fig.add_annotation(text=f"Percent Profitable:  {percent_profitable}",
                    xref='paper', yref="paper",
                    x=0.05, y=0.70, showarrow=False,
                    font=dict(size=20))
 
-# Make chart LOG Scale
-# Get rid of grid lines
-# Change background color
 
+# Make chart LOG Scale
 fig.update_yaxes(type="log", showgrid=False)
+
+# Get rid of grid lines
 fig.update_xaxes(showgrid=False)
+
+# Change background color
 fig.update_layout(template="plotly_dark")
 
+# Save figure
 plt.savefig("LINK-PROFITABLE-DAYS.png", dpi=300)
+
+# Show figure
 fig.show()
 
 
