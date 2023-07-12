605463aa8de048b86b57622a4f3b8cea4395d311
Andrew O'Drain <86028354+Drew5040@users.noreply.github.com>
2023-07-12 09:11:48 -0400

    Update monte_carlo.py

--- Python projects/monte_carlo.py
+++ Python projects/monte_carlo.py
@@ -8,7 +8,7 @@ import yfinance as yf
 import numpy as np
 
 
-# Pulls in crypto price history
+# Pull in crypto price history
 def api_call(string):
     data = yf.Ticker(string + '-USD').history(period='max', interval='1wk').reset_index()[['Date', 'Open']]
     data = data.rename(columns={'Open': string})
@@ -25,17 +25,16 @@ eth = api_call('ETH')
 final_df = pd.merge(btc, link, on='Date', how='inner')
 final_df = pd.merge(final_df, eth, on='Date', how='inner')
 
-# reset the index column for time series data
+# Reset the index column 
 final_df = final_df.set_index('Date')
 
-# Calculates the chosen period return for each coin in dataframe
+# Calculate the chosen period return for each coin in dataframe
 df_returns = final_df.pct_change()
 
 # Historical average of each crypto return in dataframe
-# Needed for calculating each portfolio return
 df_avg_returns = df_returns.mean()
 
-# Calculate the historical covariance matrix of returns that includes each crypto in dataframe
+# Calculate the historical covariance matrix of returns 
 df_cov_matrix = df_returns.cov()
 
 # Column headers
@@ -43,8 +42,7 @@ crypto = ['BTC', 'LINK', 'ETH']
 
 num_simulations = 100000
 
-# Need a container to store simulation results in
-# Create a numpy array of zeroes using a tuple
+# Create a numpy array of zeroes to use as a container for simulation results
 simulation_results = np.zeros((4 + len(crypto) - 1, num_simulations))
 
 
@@ -59,25 +57,23 @@ for i in range(num_simulations):
     # They will be the random weightings of BTC and LINK and ETH
     random_weights = np.array(np.random.random(3))
 
-    # divide each value in the array by the sum
-    # To get the percentage weighting of each asset in the portfolio
+    # Divide each value in the array by the sum to get the percentage weighting of each asset in the portfolio
     portfolio_random_weights = random_weights / np.sum(random_weights)
 
-    # calculate each portfolios returns
+    # Calculate each portfolios returns
     portfolio_returns = np.sum(df_avg_returns * portfolio_random_weights)
 
-    # calculate the portfolio standard deviation using dot-product
+    # Calculate the portfolio standard deviation using dot-product
     portfolio_standard_deviation = np.sqrt(np.dot(portfolio_random_weights.T, np.dot(df_cov_matrix, portfolio_random_weights)))
 
-    # Populate our simulation results by indexing into our zeroes array
+    # Populate the simulation results by indexing into our zeroes array
     simulation_results[0, i] = portfolio_returns
     simulation_results[1, i] = portfolio_standard_deviation
 
-    # sharp ratio
+    # Calculate Sharp Ratio
     simulation_results[2, i] = simulation_results[0, i] / simulation_results[1, i]
 
-    # To fill the remaining values use a for loop to iterate through
-    # weights variable.
+    # To fill the remaining values use a for loop to iterate through weights variable.
     for j in range(len(portfolio_random_weights)):
         simulation_results[j + 3, i] = portfolio_random_weights[j]
       
